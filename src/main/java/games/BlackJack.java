f799315a133ea4c00376c5d89cf3169d62c5702d
Kiryl Nazarau <kiryl.nazarau@pegasus.ae>
2019-06-12 22:04:14 +0400

    step 6: adding BlackJack class and implementing the BlackJack game. Adding getCharacterFromUser() method to Choice class. Choice class refactoring

---
+++ src/main/java/games/BlackJack.java
@@ -0,0 +1,146 @@
+package games;
+
+import java.io.IOException;
+
+class BlackJack {
+
+  private static final int NUMBER_OF_PLAYERS = 2;
+  private static final int PLAYER_1_INDEX = 0;
+  private static final int CRU_INDEX = 1;
+  private static final int START_AMOUNT = 100;
+  private static final int BET = 10;
+
+  private static final int MAX_VALUE = 21;
+  private static final int MAX_CARDS_COUNT = 8;
+  private static int[] cards;
+  private static int cursor;
+  private static int[][] playersCards = new int[NUMBER_OF_PLAYERS][MAX_CARDS_COUNT];
+  private static int[] playersCursors = new int[NUMBER_OF_PLAYERS];
+  private static int[] playersMoney = {START_AMOUNT, START_AMOUNT};
+
+
+  static void main() throws IOException {
+
+    while (playersMoney[PLAYER_1_INDEX] != 0 && playersMoney[CRU_INDEX] != 0) {
+
+      initRound();
+      placeBets();
+
+      addCard2Player(PLAYER_1_INDEX);
+      addCard2Player(PLAYER_1_INDEX);
+
+      while (sum(PLAYER_1_INDEX) < 20) {
+        boolean playerChoice;
+        playerChoice = Choice.confirm("Do you want to take one more card?\n");
+        if (playerChoice) {
+            addCard2Player(PLAYER_1_INDEX);
+          } else break;
+      }
+
+      addCard2Player(CRU_INDEX);
+      addCard2Player(CRU_INDEX);
+
+      while (sum(CRU_INDEX) <= 17) {
+        addCard2Player(CRU_INDEX);
+      }
+
+      int playerPoints = getFinalSum(PLAYER_1_INDEX);
+      int cruPoints = getFinalSum(CRU_INDEX);
+
+      System.out.println("Your points - " + playerPoints + ", CRU's points - " + cruPoints);
+
+      determineWinner(playerPoints, cruPoints);
+
+
+    }
+
+    if (playersMoney[PLAYER_1_INDEX] > 0)
+      System.out.println("You win! Congratulations!");
+    else
+      System.out.println("You lose the game. Sorry ...");
+
+  }
+
+  private static void addCard2Player(int playerIndex) {
+    int currentCard = cards[cursor];
+    playersCards[playerIndex][playersCursors[playerIndex]] = currentCard;
+    if (playerIndex == PLAYER_1_INDEX) {
+      System.out.println("Your card is " + CardUtils.toString(playersCards[playerIndex][playersCursors[playerIndex]]));
+    } else System.out.println("CRU card is " + CardUtils.toString(playersCards[playerIndex][playersCursors[playerIndex]]));
+    cursor++;
+    playersCursors[playerIndex]++;
+  }
+
+  private static int sum(int playerIndex) {
+    int sum = 0;
+    for (int i = 0; i < playersCursors[playerIndex]; i++) {
+      sum = sum + value(playersCards[playerIndex][i]);
+    }
+    return sum;
+  }
+
+  private static int getFinalSum(int playerIndex) {
+    int finalSum = sum(playerIndex);
+    if (finalSum <= MAX_VALUE) {
+      return finalSum;
+    }
+      return 0;
+  }
+
+  //Method to take the bet amount from the players account each round
+  private static void placeBets() {
+    for (int i = 0; i < NUMBER_OF_PLAYERS; i++) {
+      playersMoney[i] = playersMoney[i] - BET;
+    }
+  }
+
+  //Method to add win to player's account. BET * 2 - because previously we deduct BET from the account
+  private static void addRoundWinToPlayer(int playerIndex) {
+    playersMoney[playerIndex] = playersMoney[playerIndex] + (BET * 2);
+  }
+
+  //Method to return the BET to accounts in a case of a draw
+  private static void returnBets() {
+    for (int i = 0; i < NUMBER_OF_PLAYERS; i++) {
+      playersMoney[i] = playersMoney[i] + BET;
+    }
+  }
+
+  //Winner determiner, add the win to the winner ammount and print the result. In draw case returns bets to players
+  private static void determineWinner(int playerPoints, int cruPoints) {
+    if(playerPoints > cruPoints) {
+      addRoundWinToPlayer(PLAYER_1_INDEX);
+      System.out.println("You win this round! You get " + BET + "$");
+    } else if (playerPoints < cruPoints) {
+      addRoundWinToPlayer(CRU_INDEX);
+      System.out.println("You lose this round! You lose " + BET + "$");
+    } else {
+      returnBets();
+      System.out.println("No one wins. It's a draw!");
+    }
+  }
+
+  private static void initRound() {
+    System.out.println("-------------------------------------------------------------------");
+    System.out.println("You have " + playersMoney[PLAYER_1_INDEX] + "$, and CRU has " + playersMoney[CRU_INDEX] + "$. We are starting new round!");
+    cards = CardUtils.getShuffledCards();
+    playersCards = new int[2][MAX_CARDS_COUNT];
+    playersCursors = new int[]{0, 0};
+    cursor = 0;
+  }
+
+  private static int value(int card) {
+    switch (CardUtils.getPar(card)) {
+      case JACK: return 2;
+      case QUEEN: return 3;
+      case KING: return 4;
+      case SIX: return 6;
+      case SEVEN: return 7;
+      case EIGHT: return 8;
+      case NINE: return 9;
+      case TEN: return 10;
+      case ACE:
+      default: return 11;
+    }
+  }
+}
--- src/main/java/games/Choice.java
+++ src/main/java/games/Choice.java
@@ -8,18 +8,28 @@ public class Choice {
 
 
   public static void main(String[] args) throws IOException {
-    System.out.println("Chose the game:\n1 - \"Slots\", 2 - \"Drunkard\"");
-    switch (System.in.read()) {
+    System.out.println("Chose the game:\n1 - \"Slots\", 2 - \"Drunkard\", 3 - \"BlackJack\"");
+    switch (getCharacterFromUser()) {
       case '1': Slot.main(); break;
       case '2': Drunkard.main(); break;
+      case '3': BlackJack.main(); break;
       default: System.out.println("There is no game with this number!");
     }
   }
 
-  static char getCharacterFromUser() throws IOException {
+  private static char getCharacterFromUser() throws IOException {
     byte[] input = new byte[1 + LINE_SEPARATOR.length()];
     if (System.in.read(input) != input.length)
       throw new RuntimeException("The user has entered an insufficient number of characters.");
     return (char) input[0];
+  }
+
+  static boolean confirm(String message) throws IOException {
+    System.out.println(message + " \"Y\" - Yes, {any other char} - No (To exit the game enter Ctrl + C)");
+    switch (getCharacterFromUser()) {
+      case 'Y':
+      case 'y': return true;
+      default: return false;
+    }
   }
 }
